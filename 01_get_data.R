561c8d16ed1fb7d11d0eec46dbdfe637b86b0e98
Tadhg Moore <tadhgm@vt.edu>
2021-07-07 13:57:07 -0400

    Updated to download files from GitHub

--- 01_get_data.R
+++ 01_get_data.R
@@ -1,36 +1,98 @@
-#Note: lake_directory need to be set prior to running this script
+lake_directory <- getwd()
+config <- yaml::read_yaml(file.path(lake_directory,"configuration","FLAREr","configure_flare.yml"))
+config$file_path$noaa_directory <- file.path(lake_directory, "forecasted_drivers", config$met$forecast_met_model)
 
-lake_directory <- getwd() # Presuming you are using an an Rproject
 
-if(!exists("lake_directory")){
-  stop("Missing lake_directory variable")
+# Create directories
+dir.create("data_raw/fcre-manual-data", showWarnings = FALSE)
+dir.create("data_raw/fcre-weir-data", showWarnings = FALSE)
+dir.create("data_raw/fcre-metstation-data", showWarnings = FALSE)
+dir.create("data_raw/fcre-catwalk-data", showWarnings = FALSE)
+
+
+#download CTD data from EDI
+download.file("https://portal.edirepository.org/nis/dataviewer?packageid=edi.200.11&entityid=d771f5e9956304424c3bc0a39298a5ce",
+              "data_raw/fcre-manual-data/CTD_final_2013_2020.csv")
+
+#download various field_data files
+download.file("https://raw.githubusercontent.com/FLARE-forecast/FCRE-data/fcre-manual-data/FCR_SSS_inflow_2013_2020.csv",
+              "data_raw/fcre-manual-data/FCR_SSS_inflow_2013_2020.csv")
+download.file("https://github.com/FLARE-forecast/FCRE-data/blob/fcre-manual-data/FCR_GLM_NLDAS_010113_123119_GMTadjusted.csv?raw=true",
+              "data_raw/fcre-manual-data/FCR_GLM_NLDAS_010113_123119_GMTadjusted.csv")
+download.file("https://github.com/FLARE-forecast/FCRE-data/blob/fcre-weir-data/FCRweir.csv?raw=true",
+              "data_raw/fcre-weir-data/FCRweir.csv")
+download.file("https://github.com/FLARE-forecast/FCRE-data/blob/fcre-manual-data/inflow_for_EDI_2013_06Mar2020.csv?raw=true",
+              "data_raw/fcre-manual-data/inflow_for_EDI_2013_06Mar2020.csv")
+download.file("https://raw.githubusercontent.com/FLARE-forecast/FCRE-data/fcre-manual-data/FCR_weir_inflow_2013_2019_20200624_allfractions_2poolsDOC.csv",
+              "data_raw/fcre-manual-data/FCR_weir_inflow_2013_2019_20200624_allfractions_2poolsDOC.csv")
+download.file("https://raw.githubusercontent.com/FLARE-forecast/FCRE-data/fcre-manual-data/FCR_spillway_outflow_SUMMED_WeirWetland_2013_2019_20200615.csv",
+              "data_raw/fcre-manual-data/FCR_spillway_outflow_SUMMED_WeirWetland_2013_2019_20200615.csv")
+download.file("https://raw.githubusercontent.com/FLARE-forecast/FCRE-data/fcre-manual-data/Catwalk_cleanedEDI.csv",
+              "data_raw/fcre-manual-data/Catwalk_cleanedEDI.csv")
+download.file("https://raw.githubusercontent.com/FLARE-forecast/FCRE-data/fcre-catwalk-data/Catwalk.csv",
+              "data_raw/fcre-catwalk-data/Catwalk.csv")
+download.file("https://raw.githubusercontent.com/FLARE-forecast/FCRE-data/fcre-catwalk-data/CAT_MaintenanceLog.txt",
+              "data_raw/fcre-catwalk-data/CAT_MaintenanceLog.txt")
+download.file("https://raw.githubusercontent.com/FLARE-forecast/FCRE-data/fcre-manual-data/Secchi_depth_2013-2019.csv",
+              "data_raw/fcre-manual-data/Secchi_depth_2013-2019.csv")
+
+
+# download FCR met data
+download.file("https://github.com/FLARE-forecast/FCRE-data/blob/fcre-metstation-data/FCRmet.csv?raw=true",
+              "data_raw/fcre-metstation-data/FCRmet.csv")
+download.file("https://github.com/FLARE-forecast/FCRE-data/blob/fcre-manual-data/met_full_postQAQC.csv?raw=true",
+              "data_raw/fcre-manual-data/met_full_postQAQC.csv")
+
+
+
+#download EDI fcr met file
+# download.file("https://portal.edirepository.org/nis/dataviewer?packageid=edi.389.5&entityid=3d1866fecfb8e17dc902c76436239431",
+#               "data_raw/Met_final_2015_2020.csv")
+
+
+#download BVR temp data from EDI
+# inUrl1  <- "https://portal.edirepository.org/nis/dataviewer?packageid=edi.725.1&entityid=9f4d77dc90db2d87e4cdec8b7584d504"
+# infile1 <- paste0(config$data_location,"/BVR_EDI_2020.csv")
+# download.file(inUrl1,infile1,method="curl")
+
+
+
+# Download NOAA Data ----
+
+source(file.path(lake_directory, "R", "noaa_download_s3.R"))
+
+# set a start and end date for NOAA forecasts and check which days are not available in local NOAA directory
+dates <- seq.Date(as.Date('2021-07-01'), as.Date(Sys.Date()), by = 'day')
+download_dates <- c()
+for (i in 1:length(dates)) {
+  fpath <- file.path(config$file_path$noaa_directory, "NOAAGEFS_1hr", "fcre", dates[i])
+  if(dir.exists(fpath)){
+    message(paste0(dates[i], ' already downloaded'))
+  }else{
+    download_dates <- c(download_dates, dates[i])
+  }
 }
 
-config <- yaml::read_yaml(file.path(lake_directory,"configuration", "observation_processing", "observation_processing.yml"))
-
-# Set working directories relative to this project
-# Don't forget to pull in the git submodules
-# In the terminal run:
-# `git submodule init`
-# `git submodule update`
-
-config$data_location <- file.path(lake_directory, "data_raw")
+download_dates <- na.omit(download_dates)
+download_dates <- as.Date(download_dates, origin = '1970-01-01')
 
 
+for (i in 1:length(download_dates)) {
+  noaa_download_s3(siteID = config$location$site_id,
+                   date = download_dates[i],
+                   cycle = '00',
+                   noaa_horizon = 16,
+                   noaa_directory = config$file_path$noaa_directory)
 
-if(!file.exists(file.path(config$data_location, config$realtime_insitu_location))){
-  stop("Missing temperature data GitHub repo")
-}
-if(!file.exists(file.path(config$data_location, config$realtime_met_station_location))){
-  stop("Missing met station data GitHub repo")
-}
-if(!file.exists(file.path(lake_directory, config$noaa_location))){
-  stop("Missing NOAA forecast GitHub repo")
-}
-if(!file.exists(file.path(config$data_location, config$manual_data_location))){
-  stop("Missing Manual data GitHub repo")
 }
 
-if(!file.exists(file.path(config$data_location, config$realtime_inflow_data_location))){
-  stop("Missing Inflow data GitHub repo")
-}
+# noaa-point is 16 day
+
+fc_files <- list.files(file.path(config$file_path$noaa_directory, config$location$site_id,
+                                 download_dates[1], "00"),
+                       full.names = TRUE)
+
+# Inspect netcdf files
+fid <- ncdf4::nc_open(fc_files[1])
+fid$dim$longitude$vals
+ncdf4::nc_close(fid)
